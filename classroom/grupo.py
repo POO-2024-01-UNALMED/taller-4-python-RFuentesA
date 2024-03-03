903cc9d471bd3f29066b29ec0b322370f1216814
RFuentesA <159492816+RFuentesA@users.noreply.github.com>
2024-03-03 12:42:34 -0500

    commit 2

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,32 +1,30 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = None
+    grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
-        self._asignaturas = asignaturas
-        self.listadoAlumnos = estudiantes
+        self._asignaturas = asignaturas or []
+        self.listadoAlumnos = estudiantes or []
+        Grupo.grado = "Grado 12"
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
-        lista.append(alumno)
-        self.listadoAlumnos = self.listadoAlumnos + lista
+    def agregarAlumno(self, alumno, lista=None):
+       
+       if type(lista) != list:
+           self.listadoAlumnos.append(alumno)
 
-    # def __str__(self):
-    #     pass
+       else:
+           lista.append(alumno)
+           self.listadoAlumnos += lista
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
+    def __str__(self):
+        return "Grupo de estudiantes: " + self._grupo
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
+    def asignarNombre(cls, nombre = "Grado 6"):
         cls.grado = nombre
