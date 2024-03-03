e731651dda598be5ae921ed7c18dea9354e42c6c
RFuentesA <159492816+RFuentesA@users.noreply.github.com>
2024-03-03 12:49:49 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,7 +3,7 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = "Grado 12"
 
-    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes = None):
         self._grupo = grupo
         self._asignaturas = asignaturas or []
         self.listadoAlumnos = estudiantes or []
@@ -13,7 +13,7 @@ class Grupo:
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=None):
+    def agregarAlumno(self, alumno, lista = None):
        
        if type(lista) != list:
            self.listadoAlumnos.append(alumno)
